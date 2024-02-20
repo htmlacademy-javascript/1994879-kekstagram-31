cdcc46f7dff9a912753f7a37209fa31eebf299e2
Evgenii Popov <webviser@mail.ru>
2024-02-20 19:40:15 +0600

    run lint =)

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,24 @@
-const checkLimit = (string, limit) => {
-    return (string.legth <= limit);
-}
+const checkLimit = (string, limit) => (string.legth <= limit);
 
 const isPalindron = (value) => {
-    const string = String(value).replaceAll(' ', '').toUpperCase();
+  const string = String(value).replaceAll(' ', '').toUpperCase();
 
-    for (let i = 0; i < Math.ceil(string.length / 2); i++) {
-        if (string.at(i) !== string.at(-1-i)) return false;
+  for (let i = 0; i < Math.ceil(string.length / 2); i++) {
+    if (string.at(i) !== string.at(-1 - i)) {
+      return false;
     }
-    return true;
-}
+  }
+  return true;
+};
 
-const extractNumbers = (value) => { 
-    let result = '';
-    const string = String(value).replaceAll(' ', '');
-    for (i = 0; i < string.length; i++) {
-        result += (Number.isInteger(+string.at(i))) ? string.at(i) : '';
-    }
-    return result.length > 0 ? +result : NaN;
-}
+const extractNumbers = (value) => {
+  let result = '';
+  const string = String(value).replaceAll(' ', '');
+  for (let i = 0; i < string.length; i++) {
+    result += (Number.isInteger(+string.at(i))) ? string.at(i) : '';
+  }
+  return result.length > 0 ? +result : NaN;
+};
 
 checkLimit('проверяемая строка', 10);
 isPalindron('Лёша на полке клопа нашёл ');
