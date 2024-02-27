76e7fc6863f6c703c76facb54a9669ea083fb157
Evgenii Popov <webviser@mail.ru>
2024-02-28 02:07:18 +0600

    рефакторинг - убрал лишние константы - деструктуризация для объекта-диапазона - синтаксис стрелочной функции для литерального объекта - упростил алгоритм функции, так как в задании не требуется усложнять

--- js/main.js
+++ js/main.js
@@ -24,65 +24,40 @@ const DEFAULT_PHOTO_DESCRIPTIONS = [
 ];
 
 const PHOTOS_LIMIT = 25;
-const PHOTO_ID_LIMIT = 25;
-const LIKES_LIMIT = 200;
-const LIKES_START = 15;
-const COMMENTS_LIMIT = 30;
-const COMMENTS_START = 0;
-const COMMENT_ID_LIMIT = 100;
-const PHRASES_LIMIT = 2;
-const AVATARS_LIMIT = 6;
-const AVATARS_START = 0;
-const DEFAULT_START = 1;
+const LIKES_RANGE = { min: 15, max: 200 };
+const COMMENTS_RANGE = { min: 0, max: 30 };
+const AVATARS_RANGE = { min: 1, max: 6 };
 
-const getRandomInRange = (min, max) => Math.round(Math.random() * (max - min) + min);
+const getRandomInRange = ({min, max}) => Math.round(Math.random() * (max - min) + min);
+const getRandomElement = (elements) => elements[getRandomInRange({ min: 0, max: elements.length - 1})];
 
-const getRandomInArray = (array, count = 1) => {
-  const result = [];
-  const limit = getRandomInRange(DEFAULT_START, count);
-  for (let i = 0; i < limit; i++) {
-    result.push(array[getRandomInRange(0, array.length - 1)]);
-  }
-  return result.join('\n');
-};
-
-const createIdInRange = (min, max) => {
-  const existingIds = [];
+const createIdCounter = () => {
+  let lastId = 0;
 
   return () => {
-    if (existingIds.length >= (max - min + 1)) {
-      return null;
-    }
-    let id = getRandomInRange(min, max);
-    while (existingIds.includes(id)) {
-      id = getRandomInRange(min, max);
-    }
-    existingIds.push(id);
-    return id;
+    lastId += 1;
+    return lastId;
   };
 };
 
-const generateCommentId = createIdInRange(DEFAULT_START, COMMENT_ID_LIMIT);
-const generatePhotId = createIdInRange(DEFAULT_START, PHOTO_ID_LIMIT);
+const generateCommentId = createIdCounter();
+const generatePhotId = createIdCounter();
 
-const newComment = () => {
-  const comment = {
-    id: generateCommentId(),
-    avatar: `img/avatar-${getRandomInRange(AVATARS_START, AVATARS_LIMIT)}.svg`,
-    message: getRandomInArray(DEFAULT_MESSAGES, PHRASES_LIMIT),
-    name: getRandomInArray(DEFAULT_AUTHORS),
-  };
-  return comment;
-};
+const newComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInRange(AVATARS_RANGE)}.svg`,
+  message: getRandomElement(DEFAULT_MESSAGES),
+  name: getRandomElement(DEFAULT_AUTHORS),
+});
 
 const newPhoto = () => {
   const id = generatePhotId();
   return {
     id,
     url: `photos/${id}.jpg`,
-    description: getRandomInArray(DEFAULT_PHOTO_DESCRIPTIONS, DEFAULT_PHOTO_DESCRIPTIONS.length),
-    likes: getRandomInRange(LIKES_START, LIKES_LIMIT),
-    comments: Array.from({ length: getRandomInRange(COMMENTS_START, COMMENTS_LIMIT) }, newComment),
+    description: getRandomElement(DEFAULT_PHOTO_DESCRIPTIONS),
+    likes: getRandomInRange(LIKES_RANGE),
+    comments: Array.from({ length: getRandomInRange(COMMENTS_RANGE) }, newComment),
   };
 };
 
