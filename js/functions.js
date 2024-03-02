fc8c6df92342be1fb9be4c89963e458604a35d81
Evgenii Popov <webviser@mail.ru>
2024-03-02 13:06:27 +0600

    5.16. Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -23,3 +23,18 @@ const extractNumbers = (value) => {
 checkLimit('проверяемая строка', 10);
 isPalindron('Лёша на полке клопа нашёл ');
 extractNumbers(1.5);
+
+const convertTimeToNumber = (time) => {
+  const [ hours, minutes ] = String(time).split(':').map((item) => +item);
+  return hours * 60 + minutes;
+};
+
+const checkMeetingFitsInTime = (dayStart, dayEnd, meetingStart, meetingMinutes) => {
+  const dayStartMinutes = convertTimeToNumber(dayStart);
+  const dayEndMinutes = convertTimeToNumber(dayEnd);
+  const meetingStartMinutes = convertTimeToNumber(meetingStart);
+
+  return meetingStartMinutes >= dayStartMinutes && (meetingStartMinutes + meetingMinutes) <= dayEndMinutes ;
+};
+
+checkMeetingFitsInTime('8:0', '10:0', '8:0', 120);
